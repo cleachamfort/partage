6407d42e5aee08403996e512cbe30900cd857461
chamfort <clea.chamfort@mines-paristech.fr>
2021-12-08 14:02:23 +0100

    commit

---
+++ snake/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chamfort/partage/snake
+# Build directory: /home/chamfort/partage/snake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
